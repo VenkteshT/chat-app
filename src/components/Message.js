83264cf056ba5d00c11a2f52dca29eae0e2f96ad
VenkteshT <venkyr929@gmail.com>
2023-08-10 13:55:10 +0530

    added padding to message

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./message.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon as I } from "@fortawesome/react-fontawesome";
 import { faHeart, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { useDispatch } from "react-redux";
 import { actions } from "../redux/slice";
@@ -8,22 +8,22 @@ export default function Message({ message, from, id, like }) {
   //
   const dispatch = useDispatch();
 
-  //
+  // like message
   function handleClick() {
     dispatch(actions.likeMessage({ id }));
   }
 
-  //
+  // delete message
   function handleMessageDelete(id) {
     dispatch(actions.deleteMessage({ id }));
   }
   //
   return (
     <div className="chat-message">
-      <FontAwesomeIcon icon={faTrash} onClick={() => handleMessageDelete(id)} />
+      <I icon={faTrash} onClick={() => handleMessageDelete(id)} />
       <p className="content">{message}</p>
       <div className="likes">
-        <FontAwesomeIcon icon={faHeart} onClick={handleClick} />
+        <I icon={faHeart} onClick={handleClick} />
         <span className="count">{like}</span>
       </div>
       <span className="from">{from}</span>
